10fd2b3905448f91bd88ef2e9b736d1aa605b427
Patricio Palladino <email@patriciopalladino.com>
2025-10-10 22:52:09 +0000

    Another try

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightSidebarTopics from "starlight-sidebar-topics";
 import partytown from "@astrojs/partytown";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://hardhat.org/",
